c6660cae360196a5231c562a849b0bfc4acef972
Coranna Howard <me@komiga.com>
2013-05-01 08:23:22 -0400

    AssetLoader: removed invalid escape sequence¹.
    1: Needed for Lua 5.2.x, which validates escape sequences, and
    which apparently the Arch Linux LÖVE uses instead of 5.1.x.

--- src/AssetLoader.lua
+++ src/AssetLoader.lua
@@ -319,7 +319,7 @@ Kind.world={
 		local dir=love.filesystem.enumerate(root_path)
 		for _, e in pairs(dir) do
 			local is_file=love.filesystem.isFile(root_path..e)
-			local name, ext=string.match(e, "(%w+)(\.wrl)")
+			local name, ext=string.match(e, "(%w+)(.wrl)")
 			--[[Util.debug(
 				"Kind.world.preload:", is_file, name, ext
 			)--]]
